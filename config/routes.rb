177f0a0dc24ea14dd468b27f62e477aa9094013e
gcalquin <gcalquin@gmail.com>
2021-09-16 17:14:23 -0300

    Cambios para heroku mas un navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :kinds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root :to => 'bookmarks#index'
 
   get "get_bookmarks/:category", to: "bookmarks#getBookmarksByCategory", as: "getBookmarksByCategory"
 end
